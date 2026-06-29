5c78bb443303ee6354640c7c86b7870182788f86
mayank <knowledgesmart24@gmail.com>
2026-06-29 22:13:37 +0530

    first commit

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'app_pallete.dart';
+
+class Apptheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'feature/auth/view/pages/signup_page.dart';
+import 'core/theme/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme:Apptheme.darkThemeMode,
+      home: const SignupPage(),
+    );
+  }
+}
+
