32251884aa68c12f7a70ed73d08c8ed781464adc
mayank <knowledgesmart24@gmail.com>
2026-07-03 19:55:13 +0530

    connectd to database

--- server/main.py
+++ server/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.post("/")
-def test(request: Request):
-    print(request.body)
-    return {"message": "Hello World"}
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+@app.post("/signup")
+def signup_user(user: UserCreate):
+    #extract user data from the request
+    print(user.username)
+    print(user.email)
+    print(user.password)
+
+    pass
